8971189c8fd7d084f84a7ee6c422267e535fa1e4
Abdul Hanan <hannan160160@gmail.com>
2026-09-23 18:52:42 +0500

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="bg-[#0a0025] text-white ]:">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <div className="">
+      <footer className="bg-[#0a0025] text-white h-[10vh] ]:">
+  <div className=" py-2.5 mx-auto flex items-center sm:flex-row flex-col">
  <img className="logo-img w-40" src="logo.jpg" alt="Logo" />
 
      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Lock Pulse
